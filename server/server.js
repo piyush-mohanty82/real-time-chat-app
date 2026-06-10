a324e11c162df7da02e744c62a1f074a8fcec443
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-06-10 20:46:36 +0530

    socket servr added

--- server/server.js
+++ server/server.js
@@ -5,10 +5,38 @@ import http from 'http';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoute.js';
+import { Server } from 'socket.io';
 
 //creat express app and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+//initialize socket.io server
+export const io = new Server(server,{
+    cors: {origin: "*"}
+})
+
+//store online users
+export const userSocketMap = {};//{userId: socketId}
+
+//Socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+    //Emit online users to all connected clients
+    // extracts all online user IDs, and broadcasts that list to every connected client so the frontend can display who is online in real time.
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+    })
+})
 
 //middleware setup
 app.use(express.json({limit: '4mb'}));
