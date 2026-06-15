7a0b7406436c878ade31f3c8967946e9a35d3527
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-06-15 21:43:51 +0530

    error resolved

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -60,7 +60,7 @@ export const markMessageAsSeen = async (req,res) => {
 }
 
 // Send message to selected user
-export const sendMessage = async () => {
+export const sendMessage = async (req,res) => {
     try {
         const {text , image} = req.body;
         const receiverId = req.params.id;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -52,7 +52,7 @@ export const login = async (req,res) => {
 
 //controller to check if user is authenticated
 export const checkAuth = async (req,res) => {
-    res.json({sucess: true ,user: req.user});
+    res.json({success: true ,user: req.user});
 } 
 
 //controller to update user profile details
